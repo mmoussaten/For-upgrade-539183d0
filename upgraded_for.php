1deaf82698db3faca9da99ead1178cb7b1223a7a
marwan <marwan0320@live.nl>
2019-10-09 10:59:43 +0200

    Uitwerking van de exercise

---
+++ upgraded_for.php
@@ -0,0 +1,11 @@
+<?php
+$people = array(
+    "Sjonnie de Wiel",
+    "Herman Kaal",
+    "Henk de Steen",
+    "Inge Kerkhoven",
+    "Gert Kruiswijk",
+);
+for ($i= 0;$i <= count($people);$i++) {
+    echo $people[$i].PHP_EOL ;
+}
